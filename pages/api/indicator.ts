daef5189cb32e20e3c2bb573d982093e2fae2a69
Phong Pham <phamphong.theblue@gmail.com>
2023-08-07 02:39:23 +0700

    update mock data

--- pages/api/indicator.ts
+++ pages/api/indicator.ts
@@ -1,15 +1,10 @@
-import { readFileSync } from "fs";
 import { NextApiRequest, NextApiResponse } from "next";
-import path from "path";
-
-const title = []
+import data from '@/mock/indicators.json';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
-    const mockFolder = path.join(process.cwd(), 'mock');
-    let data = readFileSync(path.join(mockFolder, "indicators.json"), { encoding: "utf8" })
     res.status(200).json({
-      data: JSON.parse(data)
+      data: data
     })
   }
 }
